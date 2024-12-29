a33df2036917da0b49ce06838be0d081ebd2530e
Adit122022 <adity202212@gmail.com>
2024-12-29 14:08:01 +0530

    completed with responsive

--- class7/src/Components/IMageSwap.jsx
+++ class7/src/Components/IMageSwap.jsx
@@ -10,8 +10,8 @@ const IMageSwap = () => {
   
     }
   return (
-    <div className="flex justify-center gap-5 items-center w-screen h-screen bg-gray-500">
-     <div className='rounded-lg shadow-lg w-3/12 h-2/3 overflow-hidden border-2 border-gray-300'>
+    <div className="flex md:flex-row flex-col justify-center gap-5 items-center w-screen h-screen py-5 px-10 bg-gray-500">
+     <div className='rounded-lg shadow-lg md:w-3/12 h-2/3 overflow-hidden border-2 border-gray-300'>
      <img
       ref={img1Ref}
         src='https://i.pinimg.com/736x/ba/46/2c/ba462cef2003a18af1fa43774baaefd6.jpg'
@@ -20,7 +20,7 @@ const IMageSwap = () => {
       />
      </div>
      <button className=' active:scale-95 bg-slate-700 px-5 py-2 hover:bg-slate-600 rounded-xl text-white font-bold font-sans text-xl' onClick={handleSwap}> SWAP</button>
-     <div className='rounded-lg shadow-lg w-3/12 h-2/3 overflow-hidden border-2 border-gray-300'>
+     <div className='rounded-lg shadow-lg md:w-3/12 h-2/3 overflow-hidden border-2 border-gray-300'>
      <img
         ref={img2Ref}
         src='https://i.pinimg.com/736x/9c/ef/b8/9cefb8d70a53fcbe07a42e65a2018e9e.jpg'
